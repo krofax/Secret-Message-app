149f3eff6cb3b18ef3af8b1d3c02ebd58e93d2cf
Blessing Krofegha <bkrofegha@gmail.com>
2020-08-14 23:42:37 +0200

    encrypt input text

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi there')
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault()
+
+  //Take the input message
+  const input = document.querySelector('#message-input')
+
+  //Encrypt the text input so friends can share
+  const encrypted = btoa(input.value);
+  document.querySelector('#link-input').value = encrypted;
+})
